0a3e20160217ca8aef64f9a00480d307a1f0f9ac
Serhat Polat <132306641+polatserhat@users.noreply.github.com>
2024-08-05 19:48:43 +0300

    Initial Commit

---
+++ FilmApp-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FilmApp-iOS
+//
+//  Created by Serhat  on 05.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FilmApp-iOS/FilmApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmApp_iOSApp.swift
+//  FilmApp-iOS
+//
+//  Created by Serhat  on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FilmApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
